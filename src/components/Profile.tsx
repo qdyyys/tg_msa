183d4dc8658124b2fba0c9a3e6821876f5f62a1e
qdyyys <qdyyys@gmail.com>
2024-06-10 21:44:38 +0300

    fixinitdata

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react";
 
+interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  language_code: string;
+  is_premium: boolean;
+  allows_write_to_pm: boolean;
+}
+
 const Profile = () => {
-  const [userName, setInitData] = useState<string | null>(null);
+  const [userName, setUserName] = useState<string | null>(null);
+  const [userData, setUserData] = useState<User | null>(null);
 
   useEffect(() => {
     const tg = (window as any).tg;
     if (tg) {
       console.log("Telegram Webapp: ", tg);
-      setInitData(tg.initData);
-      console.log(tg.initData);
+      const urlParams = new URLSearchParams(tg.initData);
+      const userString = urlParams.get("user");
+      if (userString) {
+        const user = JSON.parse(decodeURIComponent(userString));
+        setUserData(user);
+        setUserName(user.first_name);
+      }
     } else {
       console.log("Telegram WebApp is not available");
     }
@@ -19,9 +35,17 @@ const Profile = () => {
       <h2 className="font-SFProRounded600 text-3xl text-center my-5">
         {`Мой профиль ${userName}`}
       </h2>
-      <div className="bg-white w-full drop-shadow-contOp min-h-16 rounded-xl px-5 py-4">
-        asdsad
-      </div>
+      {userData && (
+        <div className="bg-white w-full drop-shadow-contOp min-h-16 rounded-xl px-5 py-4">
+          <p>ID: {userData.id}</p>
+          <p>Имя: {userData.first_name}</p>
+          <p>Фамилия: {userData.last_name}</p>
+          <p>Username: {userData.username}</p>
+          <p>Язык: {userData.language_code}</p>
+          <p>Premium: {userData.is_premium ? "Yes" : "No"}</p>
+          <p>PM доступ: {userData.allows_write_to_pm ? "Yes" : "No"}</p>
+        </div>
+      )}
     </div>
   );
 };
