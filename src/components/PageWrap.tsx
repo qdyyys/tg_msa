72241c360fdb53be4daa93e4567c08727c0cb3e0
qdyyys <qdyyys@gmail.com>
2024-06-10 17:19:18 +0300

    profile_addone

--- src/components/PageWrap.tsx
+++ src/components/PageWrap.tsx
@@ -3,11 +3,56 @@ import { AiFillHome } from "react-icons/ai";
 import { IoCashOutline } from "react-icons/io5";
 import { GrTransaction } from "react-icons/gr";
 import { CgProfile } from "react-icons/cg";
+import ModelImg from "../../public/images/image 1.jpg";
+import Profile from "./Profile";
 
 const PageWrap = () => {
   return (
     <div className="max-w-xl h-screen flex flex-col">
-      <div className="bg-headColor w-full h-24 flex-none drop-shadow-headShadow"></div>
+      <div className="bg-headColor w-full h-24 flex-none drop-shadow-headShadow overflow-y-hidden overflow-x-auto">
+        <ul className="flex flex-row items-center gap-14 justify-center h-full overflow-auto">
+          <li className="flex flex-col items-center">
+            <img
+              src={ModelImg}
+              alt=""
+              className="rounded-full max-w-12 cursor-pointer active:scale-95 transition duration-150"
+            />
+            <p className="text-md text-nowrap">Модель 1</p>
+          </li>
+          <li className="flex flex-col items-center">
+            <img
+              src={ModelImg}
+              alt=""
+              className="rounded-full max-w-12 cursor-pointer active:scale-95 transition duration-150"
+            />
+            <p className="text-md text-nowrap">Модель 1</p>
+          </li>
+          <li className="flex flex-col items-center">
+            <img
+              src={ModelImg}
+              alt=""
+              className="rounded-full max-w-12 cursor-pointer active:scale-95 transition duration-150"
+            />
+            <p className="text-md text-nowrap">Модель 1</p>
+          </li>
+          <li className="flex flex-col items-center">
+            <img
+              src={ModelImg}
+              alt=""
+              className="rounded-full max-w-12 cursor-pointer active:scale-95 transition duration-150"
+            />
+            <p className="text-md text-nowrap">Модель 1</p>
+          </li>
+          <li className="flex flex-col items-center">
+            <img
+              src={ModelImg}
+              alt=""
+              className="rounded-full max-w-12 cursor-pointer active:scale-95 transition duration-150"
+            />
+            <p className="text-md text-nowrap">Модель 1</p>
+          </li>
+        </ul>
+      </div>
 
       <div className="min-h-11 flex-1">
         <Deposit />
---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+  return (
+    <div className="h-full">
+      <h2 className="font-SFProRounded600 text-3xl text-center my-5">
+        Мой профиль
+      </h2>
+    </div>
+  );
+};
+
+export default Profile;
