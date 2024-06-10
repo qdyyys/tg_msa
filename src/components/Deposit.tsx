07e263c1ec16079be791646ccf960dd9a3d4d6f7
qdyyys <qdyyys@gmail.com>
2024-06-10 16:58:45 +0300

    deposit_links

--- src/components/Deposit.tsx
+++ src/components/Deposit.tsx
@@ -7,6 +7,7 @@ const Deposit = () => {
   return (
     <div className="font-SFProRounded600 text-3xl text-center overflow-auto h-full">
       <h2 className="my-5">Пополнить баланс</h2>
+
       <div>
         <ul className="text-xs text-lightGray dep_list flex flex-col gap-2">
           <li>
