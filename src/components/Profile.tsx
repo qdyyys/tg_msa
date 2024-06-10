9e63265b2f8582247ca35d8c45555fa46ad540c3
qdyyys <qdyyys@gmail.com>
2024-06-10 21:34:55 +0300

    initdata_test

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Profile = () => {
+  const [initData, setInitData] = useState<string | null>(null);
+
   useEffect(() => {
     const tg = (window as any).tg;
-    console.log(tg + "1123123");
-  });
+    if (tg) {
+      console.log("Telegram Webapp: ", tg);
+      setInitData(tg.initData);
+    } else {
+      console.log("Telegram WebApp is not available");
+    }
+  }, []);
 
   return (
     <div className="h-full px-5">
       <h2 className="font-SFProRounded600 text-3xl text-center my-5">
-        Мой профиль
+        {`Мой профиль ${initData}`}
       </h2>
       <div className="bg-white w-full drop-shadow-contOp min-h-16 rounded-xl px-5 py-4">
         asdsad
