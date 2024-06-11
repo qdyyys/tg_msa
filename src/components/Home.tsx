822c22266b59099c7fe6fe01355285762274f7a2
qdyyys <qdyyys@gmail.com>
2024-06-11 13:56:21 +0300

    usertheme

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,67 +5,67 @@ const Home = () => {
     <div className="h-full w-full overflow-auto">
       <div className="grid grid-cols-2 w-full justify-center py-12 gap-y-8">
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
 
         <div className="flex flex-col items-center justify-center animate-showProducts">
-          <img src={ModelImage} alt="" />
+          <img src={ModelImage} alt="" className="rounded-2xl" />
           <h3 className="font-SFProDisplay500 text-xs">Наименование услуги</h3>
           <p className="font-SFProDisplay400 text-base">3000 ₽</p>
         </div>
